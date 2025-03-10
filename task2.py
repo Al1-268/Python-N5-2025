b48d9ba85a1c4535962aac64b0ecd23fdb025360
Ali Radwan <A.Radwan@albynschool.org>
2025-03-10 09:18:47 +0000

    .

---
+++ task2.py
@@ -0,0 +1,7 @@
+username = input("please enter your username: ")
+password = input("please enter your password: ")
+if username == "ian" and password == "letmein":
+    print("logon accepted")
+else:
+    print("login failed")
+    #it will be incorrect as they arent exactly the same as the username and password are different

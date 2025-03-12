266e5b67dd8c156afd69fdd83b0a727df8d1a4a2
Ali Radwan <A.Radwan@albynschool.org>
2025-03-12 14:59:28 +0000

    .

---
+++ loops3.py
@@ -0,0 +1,8 @@
+# Program 3 - Investigate and modify
+hour = 0
+while hour < 12:
+  print("The time is",hour,"o'clock.")
+  hour = hour + 1
+# What does this program do?
+# This program prints the time in hours from 0 to 11.
+# What is the purpose of the variable hour? 

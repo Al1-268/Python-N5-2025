2f85f8f3f7a5b98f3524f22e5e27b1ef07f28694
Ali Radwan <A.Radwan@albynschool.org>
2025-03-12 15:08:41 +0000

    1

--- loops4.py
+++ loops4.py
@@ -1,5 +1,10 @@
 # Program 4 - Investigate and modify
-rightnumber = 8
+import random
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+random.shuffle(arr)
+rightnumber = random.choice(arr)
+
 guess = 1
 number = int(input("Guess a number: "))
 while (guess < 3) and (number != rightnumber):

a7dc076bdb7ba26b5a4b4c003a4f06cae55364fc
Ali Radwan <A.Radwan@albynschool.org>
2025-06-18 14:57:22 +0100

    .

--- python/task3.py
+++ python/task3.py
@@ -1,4 +1,4 @@
 celcius_temp = float(input("Enter temperature in Celsius: "))
 fahrenheit_temp = (celcius_temp * 9/5) + 32
-print("The temperature in Fahrenheit is: {fahrenheit_temp}°F")
-print("The temperature in Celsius is: {celcius_temp}°C")
+print("The temperature in Fahrenheit is:", fahrenheit_temp,"°F")
+print("The temperature in Celsius is:", celcius_temp,"°C")
